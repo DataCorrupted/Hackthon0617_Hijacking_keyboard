37226eaff0cb663aeaf980a44906007744509e11
jianxiong Cai <caijx@shanghaitech.edu.cn>
2017-06-18 09:25:33 +0800

    receive2.py: working screen sharing with high delay
    Signed-off-by: jianxiong Cai <caijx@shanghaitech.edu.cn>

--- download.py
+++ download.py
@@ -2,12 +2,17 @@ import os, time
 while(1):
     # delete previous file
     try:
-        os.unlink("./image.png")
+        os.unlink("./image1.png")
     except:
         pass
 
     # download the file
-    os.system("curl 192.168.9.20/image.png > ./image.png")
+    a = os.system("curl 192.168.9.20/image.png > ./image1.png")
+    print(a)
+    if (a == 0):
+    	os.system("mv ./image1.png ./image.png")
+
+    exit(0)
 
     # sleep
-    time.sleep(0.5)
+    time.sleep(0.1)
---
+++ screen2.py
@@ -0,0 +1,130 @@
+
+import evdev
+from evdev import *
+import os
+from socket import *
+import  gtk ,gobject
+import time
+buf=1024
+def receive():
+	# Receive data anytime. If no data, sleep / wait / do anything but return.
+	(data, addr) = UDPSock.recvfrom(buf)
+	return data
+
+#!/usr/bin/env python
+# fullScreenScale.py - Show image scaled to full screen
+
+def fitRect(thing, box): 
+# scale 2
+	scaleX=float(box.width)/thing.width
+	scaleY=float(box.height)/thing.height
+	scale=min(scaleY, scaleX) 
+	thing.width=scale*thing.width 
+	thing.height=scale*thing.height
+# center 5
+	thing.x=box.width/2-thing.width/2
+	thing.y=box.height/2-thing.height/2 
+	return thing 
+
+def scaleToBg(pix, bg): 
+	fit=fitRect( 
+		gtk.gdk.Rectangle(0,0, pix.get_width(), pix.get_height()),
+		gtk.gdk.Rectangle(0,0, bg.get_width(), bg.get_height())
+		)
+	scaled=pix.scale_simple(fit.width, fit.height, gtk.gdk.INTERP_BILINEAR)
+	ret=bg.copy() 
+	scaled.copy_area(
+		src_x=0, src_y=0,
+		width=fit.width, height=fit.height, 
+		dest_pixbuf=ret, 
+		dest_x=fit.x, dest_y=fit.y) 
+	return ret 
+
+def newPix(width, height, color=0x000000ff): 
+	pix=gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB, True, 8, width , height) 
+	pix.fill(color) 
+	return pix 
+
+def getScreenShot():
+	image = ImageGrab.grab();
+	image.show();
+
+host = ""
+port = 13000
+buf = 1024
+addr = (host, port)
+UDPSock = socket(AF_INET, SOCK_DGRAM)
+UDPSock.bind(addr)
+
+path="./image.jpg"
+def receiveAndSaveImage():
+	data=receive()
+	f=open("image.jpg","w")
+	try:
+		while(data):
+			f.write(data);
+			UDPSock.settimeout(2)
+			data=receive()
+	except timeout:
+		f.close()
+		print "done\n\n\n" 
+cnt=0
+
+
+def setup():
+	try:
+		pix=gtk.gdk.pixbuf_new_from_file(os.path.join("./image0.png")) 
+		window = gtk.Window()
+		window.connect("destroy", gtk.main_quit)
+		window.fullscreen() 
+		bg=newPix(gtk.gdk.screen_width(), gtk.gdk.screen_height())
+		pixFitted=scaleToBg(pix, bg) 
+		image=gtk.image_new_from_pixbuf(pixFitted) 
+		window.add(image)
+		window.show_all()
+	except:
+		update()
+		setup()
+
+def update():
+	global cnt, window
+	while(1):
+		a = os.system("curl 192.168.9.20/image.png > ./image.png")
+		if (a == 0):
+			cnt+=1
+			os.system("cp ./image.png  ./image0.png")
+			os.system("mv ./image.png  ./image1.png")
+			time.sleep(1)
+			break;
+
+def callback():
+	global cnt,window
+
+	# update the image
+	update()
+	# download the file
+	
+
+	path="./image%s.png"%(cnt%2)
+	#receiveAndSaveImage() 
+	gtk.main_quit()
+	setup()
+	# pix=gtk.gdk.pixbuf_new_from_file(os.path.join(path)) 
+	# bg=newPix(gtk.gdk.screen_width(), gtk.gdk.screen_height())
+	# pixFitted=scaleToBg(pix, bg) 
+	# image=gtk.image_new_from_pixbuf(pixFitted) 
+	# window.clear()
+	# window.add(image)
+	# window.show_all()
+	gobject.timeout_add(100,callback)
+	gtk.main()
+
+def main(): 
+	setup()
+
+	gobject.timeout_add(1,callback)
+	gtk.main()
+
+if __name__ == "__main__":
+	main()
+
