cf93cc4c735b45751336cef52c3c2113dc8c457d
peter <PeterRong96@gmail.com>
2017-06-18 11:37:38 +0800

    sceenSender

--- screenshotAndSend.py
+++ screenshotAndSend.py
@@ -12,12 +12,7 @@ UDPSock = socket(AF_INET, SOCK_DGRAM)
 buf=1024
 while(True):
     image = ImageGrab.grab();
-    image.save("image",format="png")
-    with open("image.png","r") as f:
-        data=f.read(buf)
-        while(data):
-            if(UDPSock.sendto(data,addr)):
-                data=f.read(buf)
+    image.save("/var/www/html/image.png",format="png")
     time.sleep(5)
 
 
